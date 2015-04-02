665ab054ec25793d86aa3969d1b45f33b29395c2
Patryk <Patryk@ASUS-Patryk>
2015-04-02 14:01:42 +0200

    fix bug: noticecontroller (mesage view)

--- beck_app/src/main/java/com/beck/beck_app/controller/NoticeController.java
+++ beck_app/src/main/java/com/beck/beck_app/controller/NoticeController.java
@@ -19,7 +19,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-@Named("noticeControllersk")
+@Named("noticeController")
 @SessionScoped
 public class NoticeController implements Serializable {
 
